3d100fa2c6e9fead4c801f8baa78bb1f1a9baa8f
BMWAdam <96652874+BMWAdam@users.noreply.github.com>
2026-05-05 21:44:02 +0200

    Clean up deploy.sh by removing commented line
    Removed commented option from deploy script.

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ trap "rm -rf $temp" EXIT
 mkdir -p "$temp/etc/sops/age"
 cp ~/.config/sops/age/cetus.txt "$temp/etc/sops/age/cetus.txt"
 chmod 600 "$temp/etc/sops/age/cetus.txt"
-# --option pure-eval false 
+
 nix run --extra-experimental-features 'nix-command flakes' github:nix-community/nixos-anywhere -- --extra-files "$temp" --flake .#server1 -i ~/.ssh/id_ed25519_mpbox root@178.104.211.77
 ssh-keygen -R 178.104.211.77
